b35ece43ea4636faf83fd133dba90eee9267332e
Alexey Z <47769319+Overman775@users.noreply.github.com>
2020-04-21 04:08:35 +0500

    loader for single yaml file

--- lib/src/yaml_asset_loader.dart
+++ lib/src/yaml_asset_loader.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:ui';
 
 import 'package:flutter/services.dart';
@@ -5,12 +6,10 @@ import 'package:yaml/yaml.dart';
 
 import 'asset_loader.dart';
 
-import 'package:easy_localization/src/easy_localization_app.dart' show localeToString;
+import 'package:easy_localization/src/easy_localization_app.dart'
+    show localeToString;
 
-//
-//
-//
-//
+//Loader for multiple yaml files
 class YamlAssetLoader extends AssetLoader {
   String getLocalePath(String basePath, Locale locale) {
     return '$basePath/${localeToString(locale, separator: "-")}.yaml';
@@ -20,20 +19,36 @@ class YamlAssetLoader extends AssetLoader {
   Future<Map<String, dynamic>> load(String path, Locale locale) async {
     var localePath = getLocalePath(path, locale);
     YamlMap yaml = loadYaml(await rootBundle.loadString(localePath));
-    return _convertYamlMapToMap(yaml);
+    return convertYamlMapToMap(yaml);
   }
+}
 
-  /// Convert YamlMap to Map
-  Map<String, dynamic> _convertYamlMapToMap(YamlMap yamlMap) {
-    final map = <String, dynamic>{};
+//Loader for single yaml file
+class YamlSingleAssetLoader extends AssetLoader {
+  Map yamlData;
 
-    for (final entry in yamlMap.entries) {
-      if (entry.value is YamlMap || entry.value is Map) {
-        map[entry.key.toString()] = _convertYamlMapToMap(entry.value);
-      } else {
-        map[entry.key.toString()] = entry.value.toString();
-      }
+  @override
+  Future<Map<String, dynamic>> load(String path, Locale locale) async {
+    if (yamlData == null) {
+      yamlData =
+          convertYamlMapToMap(loadYaml(await rootBundle.loadString(path)));
+    } else {
+      log('easy localization: Yaml already loaded, read cache');
     }
-    return map;
+    return yamlData[locale.toString()];
   }
+}
+
+/// Convert YamlMap to Map
+Map<String, dynamic> convertYamlMapToMap(YamlMap yamlMap) {
+  final map = <String, dynamic>{};
+
+  for (final entry in yamlMap.entries) {
+    if (entry.value is YamlMap || entry.value is Map) {
+      map[entry.key.toString()] = convertYamlMapToMap(entry.value);
+    } else {
+      map[entry.key.toString()] = entry.value.toString();
+    }
+  }
+  return map;
 }
