282f439d405407d603b214aadb25df2ab3cd11d2
Alexey Z <47769319+Overman775@users.noreply.github.com>
2020-04-21 23:27:10 +0500

    xml parser, readme

--- lib/easy_localization_loader.dart
+++ lib/easy_localization_loader.dart
@@ -4,4 +4,5 @@ export 'package:easy_localization_loader/src/json_asset_loader.dart';
 export 'package:easy_localization_loader/src/http_asset_loader.dart';
 export 'package:easy_localization_loader/src/csv_asset_loader.dart';
 export 'package:easy_localization_loader/src/yaml_asset_loader.dart';
+export 'package:easy_localization_loader/src/xml_asset_loader.dart';
 export 'package:easy_localization_loader/src/tests_asset_loader.dart';
