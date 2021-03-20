abe6c5d622cd14b1c2be317ea5ab946f8b9225d3
Biagio Robert Pappalardo <biagiorobert@gmail.com>
2021-03-20 17:25:59 +0100

    Added null safety, updated dependencies

--- lib/src/xml_asset_loader.dart
+++ lib/src/xml_asset_loader.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 import 'dart:ui';
 
 import 'package:flutter/services.dart';
-import 'package:xml/xml.dart' as xml;
+import 'package:xml/xml.dart';
 
 import 'asset_loader.dart';
 
@@ -16,36 +16,38 @@ class XmlAssetLoader extends AssetLoader {
   Future<Map<String, dynamic>> load(String path, Locale locale) async {
     var localePath = getLocalePath(path, locale);
     log('easy localization loader: load xml file $localePath');
-    var doc = xml.parse(await rootBundle.loadString(localePath));
+    var doc = XmlDocument.parse(await rootBundle.loadString(localePath));
     doc.normalize();
-    return convertXmlNodeToMap(doc.lastChild);
+    assert(doc.lastChild != null, 'The XML file does not contain any node');
+    return convertXmlNodeToMap(doc.lastChild!);
   }
 }
 
 //Loader for single xml file
 class XmlSingleAssetLoader extends AssetLoader {
-  Map xmlData;
+  Map? xmlData;
 
   @override
   Future<Map<String, dynamic>> load(String path, Locale locale) async {
     if (xmlData == null) {
       log('easy localization loader: load xml file $path');
-      var doc = xml.parse(await rootBundle.loadString(path));
+      var doc = XmlDocument.parse(await rootBundle.loadString(path));
       doc.normalize();
-      xmlData = convertXmlNodeToMap(doc.lastChild);
+      assert(doc.lastChild != null, 'The XML file does not contain any node');
+      xmlData = convertXmlNodeToMap(doc.lastChild!);
     } else {
       log('easy localization loader: Xml already loaded, read cache');
     }
-    return xmlData[locale.toString()];
+    return xmlData![locale.toString()];
   }
 }
 
 /// Convert xmlNode to Map
-Map<String, dynamic> convertXmlNodeToMap(xml.XmlNode xmlNode) {
+Map<String, dynamic> convertXmlNodeToMap(XmlNode xmlNode) {
   final map = <String, dynamic>{};
 
   for (final entry in xmlNode.children) {
-    if (entry is xml.XmlElement) {
+    if (entry is XmlElement) {
       switch (entry.children.length) {
         case 1:
           map[entry.name.toString()] = entry.text;
