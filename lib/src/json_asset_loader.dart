ec21413e8486b625c999df2cd0370bd55933b487
Maurizio Pinotti <maurizio.pinotti@mobimentum.it>
2021-05-15 14:55:05 +0200

    Added missing JsonAssetLoader implementation

--- lib/src/json_asset_loader.dart
+++ lib/src/json_asset_loader.dart
@@ -1,13 +1,38 @@
+import 'dart:convert';
+import 'dart:developer';
 import 'dart:ui';
+
+import 'package:flutter/services.dart';
 
 import 'asset_loader.dart';
 
-//
-//
-//
-//
 class JsonAssetLoader extends AssetLoader {
+  String getLocalePath(String basePath, Locale locale) {
+    return '$basePath/${localeToString(locale, separator: "-")}.json';
+  }
+
   @override
-  Future<Map<String, dynamic>> load(String path, Locale locale) =>
-      Future.value({});
+  Future<Map<String, dynamic>> load(String path, Locale locale) async {
+    var localePath = getLocalePath(path, locale);
+    log('easy localization loader: load json file $localePath');
+
+    return jsonDecode(await rootBundle.loadString(localePath));
+  }
+}
+
+// Loader for single json file
+class JsonSingleAssetLoader extends AssetLoader {
+  Map? jsonData;
+
+  @override
+  Future<Map<String, dynamic>> load(String path, Locale locale) async {
+    if (jsonData == null) {
+      log('easy localization loader: load json file $path');
+      jsonData = jsonDecode(await rootBundle.loadString(path));
+    } else {
+      log('easy localization loader: json already loaded, read cache');
+    }
+
+    return jsonData![locale.toString()];
+  }
 }
