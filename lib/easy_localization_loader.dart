133cbe01ddc7328593a009b8fc05f3d26b21c1f3
Arief Wijaya <ariefwiijaya@gmail.com>
2021-11-10 08:18:14 +0700

    export new loader

--- lib/easy_localization_loader.dart
+++ lib/easy_localization_loader.dart
@@ -1,4 +1,5 @@
 library easy_localization_loader;
+
 export 'package:easy_localization_loader/src/file_asset_loader.dart';
 export 'package:easy_localization_loader/src/json_asset_loader.dart';
 export 'package:easy_localization_loader/src/http_asset_loader.dart';
@@ -6,3 +7,4 @@ export 'package:easy_localization_loader/src/csv_asset_loader.dart';
 export 'package:easy_localization_loader/src/yaml_asset_loader.dart';
 export 'package:easy_localization_loader/src/xml_asset_loader.dart';
 export 'package:easy_localization_loader/src/tests_asset_loader.dart';
+export 'package:easy_localization_loader/src/smart_network_asset_loader.dart';
