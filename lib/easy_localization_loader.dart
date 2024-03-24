25d5ec8976d4452a3ac5d2f899e747c8474acdcc
Arief Wijaya <ariefwiijaya@gmail.com>
2024-03-25 02:00:18 +0700

    feat: add web support

--- lib/easy_localization_loader.dart
+++ lib/easy_localization_loader.dart
@@ -2,7 +2,8 @@ export 'package:easy_localization_loader/src/csv_asset_loader.dart';
 export 'package:easy_localization_loader/src/file_asset_loader.dart';
 export 'package:easy_localization_loader/src/http_asset_loader.dart';
 export 'package:easy_localization_loader/src/json_asset_loader.dart';
-export 'package:easy_localization_loader/src/smart_network_asset_loader.dart';
+export 'package:easy_localization_loader/src/smart_network_asset_loader.dart'
+    if (dart.library.html) 'package:easy_localization_loader/src/smart_network_asset_loader_web.dart';
 export 'package:easy_localization_loader/src/tests_asset_loader.dart';
 export 'package:easy_localization_loader/src/xml_asset_loader.dart';
 export 'package:easy_localization_loader/src/yaml_asset_loader.dart';
